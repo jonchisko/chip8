2d4e0ac6020730115a2c3f5711736af4b06ff56d
jon škoberne <jonchisko@gmail.com>
2021-12-25 11:49:27 +0100

    Implement base cpu struct
    Since the assignment is simple enough, base struct CPU will hold all the necessary primitives.

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,4 +1,36 @@
 
+
+mod instructions;
+
+
+
+
 struct Cpu {
-    
+    memory: [u8; 4096],
+    registers_general: [u8; 16],
+    register_i: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+    pc: u16,
+    sp: u8,
+    stack: [u16; 16],
+    display: [u32; 64 * 32],
+    keypad: [u8; 16],
+}
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu { 
+            memory: [0u8; 4096],
+            registers_general: [0u8; 16], 
+            register_i: 0u16, 
+            delay_timer: 0u8,
+            sound_timer: 0u8, 
+            pc: 0x200, 
+            sp: 0u8, 
+            stack: [0u16; 16], 
+            display: [0u32; 64 * 32],
+            keypad: [0u8; 16],
+        }
+    }
 }
